73417bb819cec76976da21e6ca0d046991e89f9c
louis_lue <lvning126@126.com>
2015-06-04 13:39:04 +0800

    Add check server.

--- routes/test.js
+++ routes/test.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var wechat = require('wechat-enterprise');
+var WXBizMsgCrypt = require('wechat-crypto');
 
 var config = {
     token: 'KrGV',
@@ -8,9 +8,14 @@ var config = {
     corpId: 'wxe58a0875744a70fa'
 };
 
-router.get('/', wechat(config, function(req, res, next){
-    res.writeHead(200);
-    res.end("hello node weixin api.");
-}));
+router.get('/', function(req, res){
+    var msg_signature = req.query.msg_signature;
+    var timestamp = req.query.timestamp;
+    var nonce = req.query.nonce;
+    var echostr = req.query.echostr;
+    var cryptor = new WXBizMsgCrypt(config.token, config.encodingAESKey, config.corpId)
+    var s = cryptor.decrypt(echostr);
+    res.send(s.message);
+});
 
 module.exports = router;
